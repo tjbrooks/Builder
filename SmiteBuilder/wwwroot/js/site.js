fa68276f258e1f31cb2fceed897897dc8a8f3e35
tjbrooks <tbrooks0112@gmail.com>
2018-03-01 22:40:47 -0600

    Better Item name frames. Fix for missing item images (only one image saved locally).

--- SmiteBuilder/wwwroot/js/site.js
+++ SmiteBuilder/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 $(document).ready(function () {
     $('[data-toggle="popover"]').popover();
 });
+
+$("img").on("error", function () {
+    var src = $(this).attr("src");
+    src = src.slice(src.lastIndexOf("/"));
+    src = "/images" + src;
+    $(this).attr("src", src);
+});
